cce7869e8f38be07c05483996adcfd29d923b9b5
thonguyen100 <haithonguyen0305@gmail.com>
2025-09-16 13:41:02 +0700

    Fixing backend-db connection Test 1

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app._router.stack.forEach((middleware) => {
 
 // Database connection - FIXED VERSION
 const db = mysql.createPool({
-  // host: process.env.DB_HOST  || '127.0.0.1',
+  host: process.env.DB_HOST  || '10.135.0.3',
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
